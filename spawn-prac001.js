5fdf0a7a8205ffa4e5a23d3f9fb63eed3a0361c6
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-07-06 16:17:32 -0400

    Update spawn-prac001.js

--- spawn-prac001.js
+++ spawn-prac001.js
@@ -1,5 +1,7 @@
 //const { spawn } = require('child_process')
 const cp = require('child_process')
+const config = require('config')
+const srcdb = config.get('srcdb')
 
 var progs = {
     list: 'ls',
@@ -7,8 +9,24 @@ var progs = {
     mkdir: 'mkdir'
 }
 
+const ogr2ogrPath = config.get('ogr2ogrPath')
+
+var child = cp.spawn(ogr2ogrPath, [
+    '-f', 'GeoJSONSeq',
+    '-lco', 'RS=YES',
+    '/vsistdout/',
+    '-clipdst', 0, 52.4827, 5.625, 55.76573,
+    srcdb.url
+])
+child.stdout.on('data', (data) => {
+    console.log(`data:\n${data}`)
+})
+
+/*
 var child = cp.spawn(progs.list,["-a"])
 child.stdout.on('data', (data) => {
     console.log(`data:\n${data}`)
 })
+*/
+
 
