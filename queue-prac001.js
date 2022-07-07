caabac662861229f8d6cdf38576df199c25744d1
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-07-07 15:22:46 -0400

    Update queue-prac001.js

--- queue-prac001.js
+++ queue-prac001.js
@@ -1,3 +1,29 @@
+const Queue = require('better-queue')
+
+let keyInProgress = []
+
+const sleep = (wait) => {
+    return new Promise((resolve, reject) => {
+        setTimeout( () => {resolve()}, wait)
+    })
+}
+
+const queue = new Queue(async (t, cb) => {
+    const nowTime = new Date()
+    const key = t.key
+
+    keyInProgress.push(key)
+    console.log(`${keyInProgress} on process: ${nowTime} (^o^)/`)
+    await sleep(5000)
+    console.log(`${key} ends: ${nowTime} (^o^)/`)
+    keyInProgress = keyInProgress.filter((v) => !(v === key))
+    return cb()
+},{
+    concurrent: 2,
+    maxRetries: 3,
+    retryDelay: 5000
+})
+
 
 const queueTasks = () => {
     for (let key of ['a', 'b', 'c', 'd', 'e', 'f', 'g']){
